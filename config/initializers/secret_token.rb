0930c7d6b6a27fe1b7849d30ab75c47f6bb5f371
artemb1393 <darkallenb1394@gmail.com>
2012-11-13 21:04:35 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffe::Application.config.secret_token = 'e679c94dca16cef45776430b2fe32ba04e0bfc2b7d8cb64a6bae4dd0af25098c0ae261c44ccddff27ce7986baab8bd7a8fe27ed6cb37325dd95197ab2839a93c'
